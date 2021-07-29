b2df3cdc79b828055938d927eb2f05f61eed8fda
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-07-29 00:00:55 -0300

    quicksort

--- 11-Sorting Algorithms/07-QuickSort.js
+++ 11-Sorting Algorithms/07-QuickSort.js
@@ -13,10 +13,20 @@ function pivot(arr, start = 0, end = arr.length - 1) {
     }
   }
   swap(arr, start, swapIdx);
-  console.log(arr);
   return swapIdx;
 }
 
-function quickSort(arr) {}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
 const array = [2, 32, 12, 6, 7, 1];
+
+console.log(quickSort(array));
