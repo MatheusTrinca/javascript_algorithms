0a7c9b375c8cf76e6bdb3b794297001fec1c012c
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-07-30 14:18:02 -0300

    slinkedlist intro

---
+++ 12-Data Structures/01-SinglyLinkedList.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push(4);
+list.push(5);
+console.log(list);
