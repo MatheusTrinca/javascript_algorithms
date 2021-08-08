5affc4546e9fc1f7fff490a8b0153020a55f5aa4
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-08 18:42:41 -0300

    get e set

--- 12-Data Structures/01-SinglyLinkedList.js
+++ 12-Data Structures/01-SinglyLinkedList.js
@@ -14,7 +14,8 @@ class SinglyLinkedList {
     this.tail = null;
   }
 
-  push(value) {
+  // Add no final
+  push(value) { 
     const newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
@@ -27,7 +28,8 @@ class SinglyLinkedList {
     return this;
   }
 
-  pop() {
+  // Remove do final
+  pop() { 
     if(!this.head) return undefined
     let current = this.head;
     let pre = current;
@@ -45,7 +47,8 @@ class SinglyLinkedList {
     return current;
   }
 
-  shift() {
+  // Remove do Inicio
+  shift() { 
     if(!this.head) return undefined
     let current = this.head;
     this.head = current.next
@@ -55,12 +58,61 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  // Add no Inicio
+  unshift(value) { 
+    const newNode = new Node(value)
+    if(!this.head) {
+      this.tail = newNode;
+    }
+    newNode.next = this.head
+    this.head = newNode
+    this.length++;
+    return this;
+  }
+
+  // Traz o valor do nó que está no indice passado
+  get(index){
+    if(index < 0 || index >= this.length) return null;
+    let current = this.head;
+    let counter = 0
+    while(counter !== index){
+      current = current.next;
+      counter++;
+    }
+    return current;
+    // for(let i = 0; i < this.length; i++){
+    //   if(index === i){
+    //     return current.value;
+    //   }
+    //   current = current.next;
+    // }
+  }
+
+  // Muda o valor de um nó na lista, dado um indice
+  set(index, value){
+    let foundNode = this.get(index);
+    if(!foundNode) return false;
+    foundNode.value = value;
+    return true;
+  }
+
+  // add um nó em uma posição específica
+  insert(){}
 }
-
+  
 const list = new SinglyLinkedList();
-// console.log(list.push(1));
-// console.log(list.push(2));
-// console.log(list.push(3));
+console.log(list.push(1));
+console.log(list.push(2));
+console.log(list.push(3));
+
+console.log(list.get(1));
+console.log(list.set(1, 80));
+console.log(list.get(1));
+
+// console.log(list.unshift(4))
+// console.log(list.unshift(5))
+// console.log(list.unshift(6))
 
 // console.log(list.shift());
 // console.log(list);
@@ -73,6 +125,3 @@ const list = new SinglyLinkedList();
 // console.log(list.pop());
 // console.log(list.pop());
 // console.log(list.pop());
-
-
-console.log(list);
