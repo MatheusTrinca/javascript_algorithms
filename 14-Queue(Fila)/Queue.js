d90d9dd770f9da6d5bd329e44c0fe752236a645d
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-08-24 11:36:28 -0300

    Queue

--- 14-Queue(Fila)/Queue.js
+++ 14-Queue(Fila)/Queue.js
@@ -6,4 +6,43 @@ class Node {
 }
 
 // FIFO
-class Queue{}
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0
+  }
+
+  enqueue(value){
+    const newNode = new Node(value);
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    }else{
+      this.last.next = newNode;
+      this.last = newNode
+    }
+    return ++this.size
+  }
+
+  dequeue(){
+    if(!this.first) return null;
+    let temp = this.first
+    if(this.length === 1){
+      this.last = null;
+    }
+    this.first = this.first.next;
+    temp.next = null;
+    this.size--;
+    return temp;
+  }
+}
+
+const queue = new Queue()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(8)
+queue.dequeue()
+console.log(queue);
