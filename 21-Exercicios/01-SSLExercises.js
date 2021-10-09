5ca2b3fc1a6df2e2b1abc361f24e209d64fbfb93
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-10-09 11:24:15 -0300

    SSL push

---
+++ 21-Exercicios/01-SSLExercises.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+    return true;
+  }
+}
